ff5d099bdb83906caa0098853ac9a0fe15be3bf8
陈红涛 <2793682961@.com>
2023-02-08 11:21:24 +0800

    demo2

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import { createApp, VueElement } from 'vue'
+import App from './App.vue'
+import VRouter from 'vue-router'
+
+Vue.use(VRouter)
+
+import './assets/main.css'
+createApp(App).mount('#app')
